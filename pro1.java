1d2229d5436f2e7621a69777f5e2f6b1c80f4fe2
Ravishinde10 <97655844+Ravishinde10@users.noreply.github.com>
2022-01-14 10:28:25 +0530

    Add files via upload

---
+++ pro1.java
@@ -0,0 +1,8 @@
+ class assn1
+  {
+    public static void main(string [] args)
+   {
+    system.out.println("i am Ravindra Shinde")
+    }
+   }
+    
---
+++ pro2.java
@@ -0,0 +1,15 @@
+import java.java.Scanner;
+class assn2
+{
+       public static void main(string xy[])
+       {
+             int num1,num2,sum;
+             scanner sc=new Scanner(System.in);
+             System.out.println("Enter First Number");
+             num1=sc.nextln();
+             System.out.println("Enter Secound Number");
+             num2=sc.nextln();
+             sum=num1+num2;
+             System.out.println("The addition is ="+sum);
+        }
+} 
